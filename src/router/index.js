b83dbd3ff97443c43564032b0a1c5053ff1f6171
harrell <haorui658@gmail.com>
2023-04-28 16:19:40 +0800

    初始化提交

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+// router/index
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+const routes: Array<RouteRecordRaw> = 
+[  
+ {    
+    path: '/',
+    name: 'Home',
+    component: import('/@views/Home')
+ }, 
+ {    
+    path: '/user',
+    name: 'User',
+    component: () => import('/@views/User')
+ }
+]
+const router = createRouter({  
+    history: createWebHistory(process.env.BASE_URL), 
+    routes
+})
+export default router
